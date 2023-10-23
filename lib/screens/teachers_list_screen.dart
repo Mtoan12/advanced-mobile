6eceb450665b33fa38ffe0ea23d1a824e065734e
mtoan12 <20120215@student.hcmus.edu.vn>
2023-10-23 17:29:20 +0700

    bổ sung các textField và các elevetedButton filter

--- lib/screens/teachers_list_screen.dart
+++ lib/screens/teachers_list_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:lettutor/widgets/appbar.dart';
@@ -142,12 +144,123 @@ class _TeachersListScreenState extends State<TeachersListScreen> {
                             width: 4,
                           ),
                           Input(
-                            width: 120,
+                            width: 160,
                             height: 32,
                             icon: Icon(Icons.arrow_drop_down),
                             placeHolder: "selectTutorNationality".tr,
                           ),
                         ],
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Text(
+                        "selectAvailableTutoringTime".tr,
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.w500),
+                      ),
+                      Wrap(
+                        children: [
+                          Input(
+                            width: 144,
+                            height: 32,
+                            icon: Icon(
+                              Icons.calendar_month,
+                              color: Colors.grey,
+                            ),
+                            placeHolder: "selectADay".tr,
+                          ),
+                          SizedBox(
+                            width: 4,
+                          ),
+                          Input(
+                            width: 100,
+                            height: 32,
+                            icon: Icon(null),
+                            placeHolder: "startTime".tr,
+                          ),
+                          Input(
+                            width: 100,
+                            height: 32,
+                            icon: Icon(
+                              Icons.timelapse,
+                              color: Colors.grey,
+                            ),
+                            placeHolder: "endTime".tr,
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 20),
+                      Container(
+                        decoration: BoxDecoration(
+                            border: Border(
+                          bottom: BorderSide(width: 1, color: Colors.grey),
+                        )),
+                        child: Wrap(runSpacing: 8, spacing: 4, children: [
+                          Category(
+                            name: "all".tr,
+                            active: true,
+                          ),
+                          Category(
+                            name: "englishForKids".tr,
+                            active: false,
+                          ),
+                          Category(
+                            name: "englishForBusiness".tr,
+                            active: false,
+                          ),
+                          Category(
+                            name: "conversational".tr,
+                            active: false,
+                          ),
+                          Category(
+                            name: "STARTERS",
+                            active: false,
+                          ),
+                          Category(
+                            name: "MOVERS",
+                            active: false,
+                          ),
+                          Category(
+                            name: "FLYERS",
+                            active: false,
+                          ),
+                          Category(
+                            name: "KET",
+                            active: false,
+                          ),
+                          Category(
+                            name: "PET",
+                            active: false,
+                          ),
+                          Category(
+                            name: "IELTS",
+                            active: false,
+                          ),
+                          Category(
+                            name: "TOEFL",
+                            active: false,
+                          ),
+                          Category(
+                            name: "TOEIC",
+                            active: false,
+                          ),
+                          SizedBox(
+                            height: 32,
+                            child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                    padding: EdgeInsets.only(
+                                        left: 10, right: 10, top: 2, bottom: 2),
+                                    backgroundColor: Colors.white,
+                                    foregroundColor: Colors.blue[300],
+                                    side: BorderSide(
+                                        width: 1, color: Colors.blue)),
+                                onPressed: () {},
+                                child: Text(
+                                  'resetFilters'.tr,
+                                )),
+                          )
+                        ]),
                       )
                     ]),
               )
@@ -186,6 +299,7 @@ class Input extends StatelessWidget {
               borderSide: BorderSide(width: 3, color: Colors.blue),
             ),
             hintText: placeHolder,
+            hintStyle: TextStyle(fontSize: 10),
             hoverColor: Colors.blue,
             contentPadding: EdgeInsets.only(top: 0, bottom: 0, left: 12),
             border: OutlineInputBorder(
@@ -193,5 +307,32 @@ class Input extends StatelessWidget {
                 borderRadius: BorderRadius.all(Radius.circular(50))),
           ),
         ));
+  }
+}
+
+class Category extends StatefulWidget {
+  final String name;
+  final bool active;
+  const Category({super.key, required this.name, required this.active});
+
+  @override
+  State<Category> createState() => _CategoryState();
+}
+
+class _CategoryState extends State<Category> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 32,
+      child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            padding: EdgeInsets.only(left: 10, right: 10, top: 2, bottom: 2),
+            backgroundColor: widget.active ? Colors.blue[50] : Colors.grey[200],
+            foregroundColor:
+                widget.active ? Colors.blue[700] : Colors.grey[700],
+          ),
+          onPressed: () {},
+          child: Text(widget.name)),
+    );
   }
 }
