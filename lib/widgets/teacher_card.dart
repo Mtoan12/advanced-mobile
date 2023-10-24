284d4a852b15ae7627575cd31a54361c0dab0304
mtoan12 <20120215@student.hcmus.edu.vn>
2023-10-24 22:43:50 +0700

    bổ sung nút likr cho teacher card

--- lib/widgets/teacher_card.dart
+++ lib/widgets/teacher_card.dart
@@ -45,84 +45,96 @@ class TeacherCard extends StatelessWidget {
           ]),
       child: Padding(
         padding: EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: Stack(
           children: [
-            CircleAvatar(
-              radius: 40,
-              backgroundImage: AssetImage(imgUrl),
-            ),
-            Text(
-              name,
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
-            ),
-            Text(national,
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400)),
-            Row(
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Icon(
-                  Icons.star,
-                  color: Colors.yellow[600],
+                CircleAvatar(
+                  radius: 40,
+                  backgroundImage: AssetImage(imgUrl),
                 ),
-                Icon(
-                  Icons.star,
-                  color: Colors.yellow[600],
+                Text(
+                  name,
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
                 ),
-                Icon(
-                  Icons.star,
-                  color: Colors.yellow[600],
+                Text(national,
+                    style:
+                        TextStyle(fontSize: 16, fontWeight: FontWeight.w400)),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.star,
+                      color: Colors.yellow[600],
+                    ),
+                    Icon(
+                      Icons.star,
+                      color: Colors.yellow[600],
+                    ),
+                    Icon(
+                      Icons.star,
+                      color: Colors.yellow[600],
+                    ),
+                    Icon(
+                      Icons.star,
+                      color: Colors.yellow[600],
+                    ),
+                    Icon(
+                      Icons.star,
+                      color: Colors.yellow[600],
+                    ),
+                  ],
                 ),
-                Icon(
-                  Icons.star,
-                  color: Colors.yellow[600],
+                SizedBox(
+                  height: 24,
                 ),
-                Icon(
-                  Icons.star,
-                  color: Colors.yellow[600],
+                Wrap(
+                    runSpacing: 8,
+                    spacing: 4,
+                    children: filters.map((filter) {
+                      return FilterItem(name: filter, active: true);
+                    }).toList()),
+                SizedBox(
+                  height: 24,
                 ),
+                Text(
+                  description,
+                  style: TextStyle(
+                    fontSize: 14,
+                  ),
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                Align(
+                    alignment: Alignment.centerRight,
+                    child: Container(
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.white,
+                              foregroundColor: Colors.blue[500],
+                              side: BorderSide(color: Colors.blue)),
+                          onPressed: () {},
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Icon(Icons.calendar_today),
+                              SizedBox(
+                                width: 4,
+                              ),
+                              Text("book".tr),
+                            ],
+                          )),
+                    ))
               ],
             ),
-            SizedBox(
-              height: 24,
-            ),
-            Wrap(
-                runSpacing: 8,
-                spacing: 4,
-                children: filters.map((filter) {
-                  return FilterItem(name: filter, active: true);
-                }).toList()),
-            SizedBox(
-              height: 24,
-            ),
-            Text(
-              description,
-              style: TextStyle(
-                fontSize: 14,
-              ),
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            Align(
-                alignment: Alignment.centerRight,
-                child: Container(
-                  child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.white,
-                          foregroundColor: Colors.blue[500],
-                          side: BorderSide(color: Colors.blue)),
-                      onPressed: () {},
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Icon(Icons.calendar_today),
-                          SizedBox(
-                            width: 4,
-                          ),
-                          Text("book".tr),
-                        ],
-                      )),
+            Positioned(
+                right: 0,
+                child: Icon(
+                  hasLiked ? Icons.favorite : Icons.favorite_border,
+                  color: Colors.blue,
+                  size: 32,
                 ))
           ],
         ),
